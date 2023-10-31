1e808769d7a8b63ba4cc9fc68262aae14ccd593d
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2023-10-31 12:20:47 +0300

    Add files via upload

---
+++ theme8/pr3/pr_3/pr_3/Program.cs
@@ -0,0 +1,126 @@
+﻿
+//Task1
+//1
+//for (int y = 1; y <= 30;y ++)
+//{
+//    for (int x = 1;x <= 15;x++) {
+//        if (7 < x & x < 9)
+//        {
+//            Console.Write(" *");
+//        } else
+//        {
+//            Console.Write("  ");
+//        }
+//    }
+//    Console.WriteLine();
+//}
+
+//2
+//for (int y = 1; y <= 30; y++)
+//{
+
+//    for (int x = 1; x <= 15; x++)
+//    {
+//        if (y == 19 && 1 <= x && x <= 15)
+//        {
+//            Console.Write(" #");
+//        }
+//        else
+//        {
+//            Console.Write(" ");
+//        }
+//    }
+//    Console.WriteLine();
+//}
+//3
+
+//for (int y = 1; y <= 18; y++)
+//{
+//    for (int x = 1; x <= 15; x++)
+//    {
+//        if (5 < x & x < 7)
+//        {
+//            Console.Write("#");
+//        }
+//        else
+//        {
+//            Console.Write(" ");
+//        }
+//    }
+//    Console.WriteLine();
+//}
+//4
+//for (int y = 1; y <= 7; y++)
+//{
+//    for (int x = 1; x <= 15; x++)
+//    {
+//        Console.Write("#");
+//    }
+//    Console.WriteLine();
+//}
+//5
+//for (int y = 1; y <= 4; y++)
+//{
+//    for (int x = 1; x <= 7; x++)
+//    {
+//        Console.Write("#");
+//    }
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+//TAsk2
+//1
+//int start = 1;
+//int end = 10;
+//for (int y = 1; y <= 10; y++)
+//{
+//    for (int x = 1; x <= 10; x++)
+//    {
+//        if (start <= x & x <= end)
+//        {
+//            Console.Write("#");
+//        } else {
+//            Console.Write(" ");
+//        }
+//    }
+//    start++;
+//    Console.WriteLine();
+//}
+//2
+//int start = 1;
+//int end = 10;
+//for (int y = 1; y <= 10; y++)
+//{
+//    for (int x = 1; x <= 10; x++)
+//    {
+//        if (start <= x & x <= end)
+//        {
+//            Console.Write("#");
+//        }
+//        else
+//        {
+//            Console.Write(" ");
+//        }
+//    }
+//    end--;
+//    Console.WriteLine();
+//}
+//3
+//int start = 1;
+//int end = 10;
+//for (int y = 1; y <= 10; y++)
+//{
+//    for (int x = 1; x <= 10; x++)
+//    {
+//        if (start <= x & x <= end)
+//        {
+//            Console.Write(" ");
+//        }
+//        else
+//        {
+//            Console.Write("#");
+//        }
+//    }
+//    end--;
+//    Console.WriteLine();
+//}
